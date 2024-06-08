cb57737b1601fd6eecd8d99f872061c0ca9b9eaf
Jack <15710512+jackbow@users.noreply.github.com>
2024-06-08 12:43:37 -0400

    feat: center hero

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -14,7 +14,9 @@
   loading="eager"
   fetchpriority="high"
 />
-<div class="text-white absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 pl-20 pt-40 font-extrabold space-y-0">
+<div
+  class="text-white absolute top-0 text-center left-0 w-full h-full bg-black bg-opacity-30 flex flex-col justify-center items-center gap-20 pb-32 font-extrabold space-y-0"
+>
   <h1 class="text-9xl">LURKING FEAR</h1>
   <h2 class="text-4xl opacity-60">THE MELISSA RUIZ STORY</h2>
 </div>
