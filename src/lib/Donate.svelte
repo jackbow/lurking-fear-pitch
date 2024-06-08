9a444f3d6c8c8cbdde42a640a358a27f40b9952c
Jack <15710512+jackbow@users.noreply.github.com>
2024-06-08 03:15:26 -0400

    fix: givebutter script

--- src/lib/Donate.svelte
+++ src/lib/Donate.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
 </script>
 
-<svelte:head>
-  <script
-    async
-    src="https://widgets.givebutter.com/latest.umd.cjs?acct={import.meta.env.VITE_GIVE_BUTTER_ACCOUNT_ID}&p=other"
-  ></script>
-</svelte:head>
-<section id="donate" class="flex flex-col flex-wrap justify-center items-center bg-white px-20 pt-16 h-screen gap-6">
+<section id="donate" class="flex flex-col justify-center items-center bg-white px-20 pt-16 h-screen gap-6">
   <h2 class="text-8xl">Thank You</h2>
   <div class="space-y-2 text-center font-normal text-xl">
     <p>
@@ -15,9 +9,7 @@
     </p>
     <p>Let's create it.</p>
   </div>
-  <div class="">
-    <givebutter-widget id={import.meta.env.VITE_GIVE_BUTTER_WIDGET_ID}></givebutter-widget>
-  </div>
+  <givebutter-widget id={import.meta.env.VITE_GIVE_BUTTER_WIDGET_ID}></givebutter-widget>
 </section>
 
 <style lang="stylus">
