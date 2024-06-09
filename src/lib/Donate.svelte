9baa04c2824f01f30e9ec0894c8253867ef0e6fe
Jack <15710512+jackbow@users.noreply.github.com>
2024-06-09 19:50:40 -0400

    feat: meta tags & thank you text size

--- src/lib/Donate.svelte
+++ src/lib/Donate.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <section id="donate" class="flex flex-col justify-center items-center bg-white px-20 pb-12 h-screen gap-6">
-  <h2 class="text-8xl">Thank You</h2>
+  <h2 class="text-6xl sm:text-6xl md:text-7xl lg:text-8xl font-sans font-extrabold">Thank You</h2>
   <div class="space-y-2 text-center font-normal text-xl">
     <p>
       We want to see a different kind of hero in the world of adventure storytelling. It's time for a cultural shift.
