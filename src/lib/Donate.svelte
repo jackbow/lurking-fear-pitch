ffa66a4ae06cf5f2ac7c8b7a61f44a023ff61844
Jack <15710512+jackbow@users.noreply.github.com>
2024-06-09 20:52:48 -0400

    fix: donate text ios

--- src/lib/Donate.svelte
+++ src/lib/Donate.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 </script>
 
-<section id="donate" class="flex flex-col justify-center items-center bg-white px-20 pb-12 h-screen gap-6">
-  <h2 class="text-6xl sm:text-6xl md:text-7xl lg:text-8xl font-sans font-extrabold">Thank You</h2>
-  <div class="space-y-2 text-center font-normal text-xl">
+<section id="donate" class="flex flex-col justify-center items-center bg-white px-8 md:px-20 pb-12 h-screen gap-6">
+  <h2 class="text-center text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-sans font-extrabold">Thank You</h2>
+  <div class="space-y-2 text-center font-normal text-lg">
     <p>
       We want to see a different kind of hero in the world of adventure storytelling. It's time for a cultural shift.
     </p>
