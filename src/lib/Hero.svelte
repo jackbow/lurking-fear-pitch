88e9ce6b9303ea90c4dc1215aefc1d8030e613ad
Jack <15710512+jackbow@users.noreply.github.com>
2024-06-09 21:22:06 -0400

    feat: rm hero poster

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-  import hero_jpg from '~/assets/images/hero.jpg';
   import vp9 from '~/assets/videos/melissa_2_vp9.webm';
 </script>
 
-<video poster={hero_jpg} autoplay muted loop playsinline class="h-screen w-screen object-cover object-center">
+<video autoplay muted loop playsinline class="h-screen w-screen object-cover object-center">
   <source src={vp9} type="video/webm" />
 </video>
 <div
